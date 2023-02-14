45820835ad220488aa766ce8309b33c9ffa04b8d
sies2 <106228124+sies2@users.noreply.github.com>
2023-02-14 16:20:54 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,12 +5,12 @@ DIR_CONFIG="/etc/v2ray"
 DIR_RUNTIME="/usr/bin"
 DIR_TMP="$(mktemp -d)"
 
-uuidone=0e8cc52e-caf5-44aa-90c1-697e49fd8887
-uuidtwo=56a697d8-3fcd-4cf0-859c-9ab1b5df58df
-uuidthree=fbcfc0ba-76ce-4fb6-a61a-ed239b70b802
-uuidfour=50da9c1f-bf7a-47fa-b933-f7ad52161d41
-uuidfive=281204fe-1c43-4d9d-8727-b2ba90fc9863
-mypath=/file-myfile
+uuidone=3f05afb6-5982-4081-be07-813fcedf967e
+uuidtwo=af27e977-7438-4978-8790-b54de5b4a818
+uuidthree=9dc799b5-cf23-4295-a567-a4977312aaa0
+uuidfour=161db3de-97ac-405a-818b-e9881b8340b5
+uuidfive=8c0f627e-d7b2-42f0-9d77-ede3adc03032
+mypath=/fiswle-myww21le
 myport=8080
 
 
